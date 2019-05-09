dd4cf007c2c218467d8454a032b8d4f2016fe3be
Hyun Namgung <hyun996@gmail.com>
2019-05-09 09:23:05 +0900

    vim 환경 추가

--- set_env.sh
+++ set_env.sh
@@ -2,6 +2,8 @@
 
 ln -fs ~/user_env/zshrc_mac ~/.zshrc
 ln -fs ~/user_env/bash_profile_mac ~/.bash_profile
+ln -fs ~/user_env/.vimrc ~/.vimrc
+ln -fs ~/user_env/.vim ~/.vim
 
 source ~/.zshrc
 source ~/.bash_profile
