7285442b0da89a159511338f0b4ca342a5549a28
root <root@hipbone.ngh.com>
2020-03-13 05:23:45 +0900

    CentOS8 세팅을 위한 set_env.sh 스크립트 업데이트 - zsh 세팅 추가

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
-ln -fs ~/user_env/zshrc_mac ~/.zshrc
-ln -fs ~/user_env/bash_profile_mac ~/.bash_profile
-ln -fs ~/user_env/.vimrc ~/.vimrc
-ln -fs ~/user_env/.vim ~/.vim
+# Variables
+OS=`uname -s`
 
-source ~/.zshrc
-source ~/.bash_profile
+function get_os() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=${ID}
+    VER=${VERSION_ID}
+  else
+    OS=$(uname -s)
+    VER=$(uname -r)
+  fi
+}
+
+function set_zsh_for_linux() {
+  echo ''  
+}
+
+get_os
+
+case ${OS} in 
+  centos)
+    # zsh  && plugins install
+    [ ! -f $(which zsh) ] && yum install -y zsh
+    yum install -y autojump-zsh
+    # fzf install for zsh-interactive-cd
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+    ;;
+  Darwin)
+    # plugin install
+    brew install -y autojump
+    ln -fs ~/User_env/zshrc_mac ~/.zshrc
+    ln -fs ~/User_env/bash_profile_mac ~/.bash_profile
+    ln -fs ~/user_env/.vimrc ~/.vimrc
+    ln -fs ~/user_env/.vim ~/.vim
+    ;;
+esac
+
+#source ~/.zshrc
+#source ~/.bash_profile
