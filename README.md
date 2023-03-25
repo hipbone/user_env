d88163a9ab6ed53224f34d4aebb53e7781d5b26d
Hyun Namgung <hyun996@gmail.com>
2023-03-25 18:26:18 +0900

    ubuntu에서 zsh 환경 구성 스크립트 추가

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# PC Setting
+# user_env
+
+새로운 PC 및 서버에서 빠르게 사용자 환경을 구성할 수 있도록 도와주는 도구
+
+## 지원되는 배포판
+
+* MacBook(검토 필요)
+* CentOS(검토 필요)
+* Ubuntu(final)
+
+## 통합 환경 구성
+
+다음을 실행하면 zsh 환경이 구성 됨
+* 모든 배포판에서 동일하며, 현재 ubuntu만 검증했음
+
+```bash
+bash set_env.sh
+```
 
 ## MacBook
 
@@ -8,7 +25,7 @@
 bash setting_mac.sh
 ```
 
-## Linux
+## CentOS
 
 ## Vim 세팅
 
@@ -23,3 +40,5 @@ bash setting_mac.sh
 ```base
 ./set_env_linux.sh
 ```
+
+
--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Variables
-OS=`uname -s`
+## Variables
+ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
 
 function get_os() {
   if [ -f /etc/os-release ]; then
@@ -21,10 +21,18 @@ function set_zsh_for_linux() {
 # OS, VER 정보 가져오기
 get_os
 
+function requirement_package() {
+  $PKG_MANAGER install -y wget curl git zsh bat
+}
+
+
+
 case ${OS} in 
   centos)
+    PKG_MANAGER="sudo yum"
+    ZSH_FILE="zshrc_centos"
     # zsh  && plugins install
-    [ ! -f $(which zsh) ] && yum install -y zsh
+    [ ! $(which zsh) ] && yum install -y zshset_env.sh
     yum install -y autojump-zsh
     # fzf install for zsh-interactive-cd
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -34,6 +42,8 @@ case ${OS} in
     cp ~/user_env/zshrc_centos ~/.zshrc
     ;;
   Darwin)
+    PKG_MANAGER="brew"
+    ZSH_FILE="zshrc_mac"
     # plugin install
     brew install -y autojump
     ln -fs ~/User_env/zshrc_mac ~/.zshrc
@@ -41,7 +51,36 @@ case ${OS} in
     ln -fs ~/user_env/.vimrc ~/.vimrc
     ln -fs ~/user_env/.vim ~/.vim
     ;;
+  ubuntu)
+    PKG_MANAGER="sudo apt"
+    ZSH_FILE="${PWD}/zshrc_ubuntu"
+    ;;
 esac
 
-#source ~/.zshrc
-#source ~/.bash_profile
+# 필수 패키지 설치
+requirement_package "$PKG_MANAGER"
+
+## install oh-my-zsh
+if [ ! -d "${HOME}/.oh-my-zsh" ];then
+    Yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh은 이미 설치되었습니다."
+fi
+
+## powerlevel10k theme 설치
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ];then
+    git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
+fi
+
+# zsh 플러그인 설치
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ];then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+fi
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ];then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+fi
+
+# zsh symlink
+ln -fs "${ZSH_FILE}" "${HOME}"/.zshrc
+
+source "${HOME}"/.zshrc
